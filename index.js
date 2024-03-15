d39ab322b30a16e19b8830a320aa915e47f5dd14
scottaguila <scott.aguila@gmail.com>
2024-03-15 19:09:58 -0400

    Post CommentUpdated to event-bus

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.post("/posts/:id/comments", async (req, res) => {
   res.status(201).send(comments);
 });
 
-app.post("/events", (req, res) => {
+app.post("/events", async (req, res) => {
   console.log("Received Event", req.body.type);
 
   const { type, data } = req.body;
@@ -50,6 +50,16 @@ app.post("/events", (req, res) => {
       return comment.id === id;
     });
     comment.status = status;
+
+    await axios.post("http://localhost:4005/events", {
+      type: "CommentUpdated",
+      data: {
+        id,
+        status,
+        postId,
+        content,
+      },
+    });
   }
 
   res.send({});
